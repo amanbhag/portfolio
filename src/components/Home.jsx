b5516dab98ef111b16caba9d9eef422373b4b07b
amanbhag <39967513+amanbhag@users.noreply.github.com>
2023-04-26 16:47:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f] p-2 mt-10">
+    <div name="home" className="w-full h-screen bg-[#0a192f] p-2 ">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8  flex flex-col justify-center h-full">
         <p className="text-pink-600 px-2 py-2">Hi, my name is</p>
